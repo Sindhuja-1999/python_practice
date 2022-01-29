feab621477f6640a2b94aebf4981bd356d4433e8
Sindhuja-1999 <bodavula.sindhuja31@gmail.com>
2022-01-29 23:42:22 +0530

    update Run Length Encoding Code

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -1,8 +1,33 @@
 def runLengthEncoding(string):
-	for i in string:
-		q=len(i)
-		if len(i)<10:
-			return qi
+	em=""
+	
+	lst=list(string)
+	q=set(lst)
+	sub=sorted(list(q))
+	p=[]
+	for i in range(len(sub)):
+		t=lst.count(sub[i])
+		if t>9:
+			p.append(9)+p.append(t-9)
 		else:
-			return 10i+(q-9)i
+			
+			p.append(t)
+	return p	
+		
+		
+	# c=0
+	# p=None
+	# for i in lst:
+	# 	if p==None:
+	# 		p=i
+	# 	elif p==i:
+	# 		c=c+1
+			
+		
+	# for i in string:
+	# 	q=len(i)
+	# 	if len(i)<10:
+	# 		return qi
+	# 	else:
+	# 		return 9i+(q-9)i
 	
